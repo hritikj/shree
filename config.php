c077695cec5e132d92fa2555a4bce44b8943735a
Ubuntu <ubuntu@ip-172-31-26-55.ec2.internal>
2026-02-23 17:13:49 +0000

    update user and pwd

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 ﻿<?php
 $currency = '₹';
-$db_username = 'root';
-$db_password = '';
+$db_username = 'myuser';
+$db_password = 'mypassword';
 $db_name = 'shree';
 $db_host = 'localhost';
 $mysqli = new mysqli($db_host, $db_username, $db_password,$db_name);
