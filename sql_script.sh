ee65fdc3454a6ae46997638c4cec4dab66af744d
Ubuntu <ubuntu@ip-172-31-46-160.ec2.internal>
2026-02-24 19:52:52 +0000

    updated sql

--- sql_script.sh
+++ sql_script.sh
@@ -3,11 +3,14 @@
 DB_NAME="shree"
 DB_USER="myuser"
 DB_PASS="mypassword"
+SQL_PATH="/var/www/html/shree.sql"
 sudo mysql <<DB_SCRIPT
 CREATE DATABASE $DB_NAME;
 CREATE USER '$DB_USER'@'localhost' IDENTIFIED BY '$DB_PASS';
 GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'localhost';
 FLUSH PRIVILEGES;
+
+mysql -u $DB_USER -p $DB_PASS $DB_NAME < $SQL_PATH
 
 DB_SCRIPT
 echo "Success"
